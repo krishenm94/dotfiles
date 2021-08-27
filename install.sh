edaaaef1ef2c46098be1f2c421ff7b9261a426f4
Krishen Mohan <krishenm@gmail.com>
2021-08-27 23:26:05 +0800

    update scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sv $PWD/.bashrc $HOME
+ln -sv $PWD/.aliasrc $HOME
+ln -sv $PWD/.functionrc $HOME
+ln -sv $PWD/.envrc $HOME
+ln -sv $PWD/.inputrc $HOME
+
+ln -sv $PWD/.config/redshift.conf $HOME/redshift.conf
+ln -sv $PWD/.config/nvim $HOME/nvim
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -svf $HOME/Repos/dotfiles/.bashrc $HOME
-ln -svf $HOME/Repos/dotfiles/.aliasrc $HOME
-ln -svf $HOME/Repos/dotfiles/.functionrc $HOME
-ln -svf $HOME/Repos/dotfiles/.envrc $HOME
-ln -svf $HOME/Repos/dotfiles/.inputrc $HOME
