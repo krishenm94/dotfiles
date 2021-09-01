eb391f11c35397030225a5327aa56a2cfbc3bc12
Krishen Mohan <krishenm@gmail.com>
2021-09-01 21:26:26 +0800

    some clean up and case insensitive completion

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-ln -sv $PWD/.bashrc $HOME
-ln -sv $PWD/.aliasrc $HOME
-ln -sv $PWD/.functionrc $HOME
-ln -sv $PWD/.envrc $HOME
-ln -sv $PWD/.inputrc $HOME
+ln -sv "$PWD"/.bashrc "$HOME"
+ln -sv "$PWD"/.aliasrc "$HOME"
+ln -sv "$PWD"/.functionrc "$HOME"
+ln -sv "$PWD"/.envrc "$HOME"
+ln -sv "$PWD"/.inputrc "$HOME"
+ln -sv "$PWD"/.tmux.conf "$HOME"
 
 link-dir-contents(){
-    echo 'Linking files in' $1
-    ln -sv $PWD/$1/* -t $HOME/$1
+    echo 'Linking files in' "$1"
+    ln -sv "$PWD"/"$1"/* -t "$HOME"/"$1"
 }
 
 link-dir-contents .config
