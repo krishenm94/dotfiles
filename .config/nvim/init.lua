27a447245f0f5107462d8ff60ace93aab32d384e
Krishen Mohan <krishenm@gmail.com>
2025-03-10 15:13:27 +0800

    add telescope git branch shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,111 +23,117 @@ vim.keymap.set('n', '<Leader>q', '<cmd>enew<bar>bd #<CR>')
 
 ---- Toggle diagnostic window
 vim.keymap.set('n', '<Leader>i', function()
-    -- If we find a floating window, close it.
-    local found_float = false
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-        if vim.api.nvim_win_get_config(win).relative ~= '' then
-            vim.api.nvim_win_close(win, true)
-            found_float = true
-        end
+  -- If we find a floating window, close it.
+  local found_float = false
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative ~= '' then
+      vim.api.nvim_win_close(win, true)
+      found_float = true
     end
+  end
 
-    if found_float then
-        return
-    end
+  if found_float then
+    return
+  end
 
-    vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
+  vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
 end, { desc = 'Toggle Diagnostics' })
 
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Copy path to clipboard
 vim.api.nvim_create_user_command("Cppath", function()
-    local path = vim.fn.expand("%:p")
-    vim.fn.setreg("+", path)
-    vim.notify('Copied "' .. path .. '" to the clipboard!')
+  local path = vim.fn.expand("%:~:.")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.api.nvim_create_user_command("CppathAbs", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
 -- Example using a list of specs with the default options
 
 -- List Plugins
 require('lazy').setup({
-    { 'morhetz/gruvbox' },
-    { 'folke/tokyonight.nvim' },
-    { 'folke/which-key.nvim' },
-    { 'folke/neoconf.nvim',   cmd = 'Neoconf' },
-    { 'folke/neodev.nvim' },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },             -- Required
-            { 'williamboman/mason.nvim' },           -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+  { 'morhetz/gruvbox' },
+  { 'folke/tokyonight.nvim' },
+  { 'folke/which-key.nvim' },
+  { 'folke/neoconf.nvim',   cmd = 'Neoconf' },
+  { 'folke/neodev.nvim' },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' },   -- Required
-            {
-                'L3MON4D3/LuaSnip',
-                dependencies = {
-                    'saadparwaiz1/cmp_luasnip',
-                    "rafamadriz/friendly-snippets"
-                },
-            }, -- Required
-        }
-    },
-    { 'tpope/vim-fugitive' },
-    { 'tpope/vim-commentary' },
-    { 'tpope/vim-surround' },
-    { 'ThePrimeagen/harpoon' },
-    {
-        "ThePrimeagen/refactoring.nvim",
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' },   -- Required
+      {
+        'L3MON4D3/LuaSnip',
         dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
+          'saadparwaiz1/cmp_luasnip',
+          "rafamadriz/friendly-snippets"
         },
-        config = function()
-            require("refactoring").setup({})
-        end,
+      }, -- Required
+    }
+  },
+  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-commentary' },
+  { 'tpope/vim-surround' },
+  { 'ThePrimeagen/harpoon' },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
-    { 'hrsh7th/nvim-cmp' },
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'norcalli/nvim-colorizer.lua' },
-    {
-        'echasnovski/mini.nvim',
-        version = '*',
-        dependencies = {
-            "lewis6991/gitsigns.nvim",
-            "nvim-tree/nvim-web-devicons",
-        }
-    },
-    {
-        'folke/trouble.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        }
-    },
+    config = function()
+      require("refactoring").setup({})
+    end,
+  },
+  { 'hrsh7th/nvim-cmp' },
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { 'norcalli/nvim-colorizer.lua' },
+  {
+    'echasnovski/mini.nvim',
+    version = '*',
+    dependencies = {
+      "lewis6991/gitsigns.nvim",
+      "nvim-tree/nvim-web-devicons",
+    }
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    }
+  },
 })
 
 -- Setup Plugins
@@ -139,11 +145,11 @@ vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
 vim.cmd("colorscheme gruvbox")
 
 require('neoconf').setup({
-    -- override any of the default settings here
+  -- override any of the default settings here
 })
 -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
 require('neodev').setup({
-    -- add any options here, or leave empty to use the default settings
+  -- add any options here, or leave empty to use the default settings
 })
 
 require 'colorizer'.setup()
@@ -161,9 +167,9 @@ require("telescope").load_extension('harpoon')
 
 local telescope = require('telescope')
 telescope.setup {
-    defaults = {
-        layout_strategy = "vertical"
-    },
+  defaults = {
+    layout_strategy = "vertical"
+  },
 }
 
 local telescope_builtin = require('telescope.builtin')
@@ -171,6 +177,7 @@ vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gb', telescope_builtin.git_branches, {})
 
 require('nvimcmp')
 require('treesitter')
