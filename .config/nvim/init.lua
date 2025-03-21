a3ce3c7661cb16382e8c5a23182bbee1db3bc40a
Krishen Mohan <krishenm@gmail.com>
2025-03-21 12:18:39 +0800

    more git mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -177,7 +177,11 @@ vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
 vim.keymap.set('n', '<leader>gb', telescope_builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gi', telescope_builtin.git_status, {})
+vim.keymap.set('n', '<leader>gs', telescope_builtin.git_stash, {})
+vim.keymap.set('n', '<leader>gc', telescope_builtin.git_commits, {})
 
 require('nvimcmp')
 require('treesitter')
